d6490ad245161d3d1dbb5ba5c9a99c8ae9af239f
conquerer-wo <remiss_suit.4d@icloud.com>
2025-06-23 20:24:25 -0400

    set up the fortend

---
+++ frotend/app.js
@@ -0,0 +1,6 @@
+const input = document.getElementById('input');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+const center = document.getElementById('center');
+const trapezoid = document.getElementById('trapezoid');
+const butt = document.getElementById('butt')
